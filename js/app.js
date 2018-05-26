67284fd9aa65b2f474720ba12a1529da4515bb25
Elia Lima <eliaml777@gmail.com>
2018-05-26 19:03:59 -0300

    Used ObservableArray and ForEach to show the list of cats

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 var viewModel = function() {
   this.clickCount = ko.observable(0);
-  this.name = ko.observable('Tabby');
+  this.title = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-  //this.level = ko.observable('Newborn');
+  this.catArray = ko.observableArray([
+    {name: 'Tabby', imgSrc: 'img/434164568_fea0ad4013_z.jpg'},
+    {name: 'Mr. T', imgSrc: 'img/22252709_010df3379e_z.jpg'},
+    {name: 'T. Bone', imgSrc: 'img/1413379559_412a540d29_z.jpg'},
+    {name: 'Sleepyhead', imgSrc: 'img/9648464288_2516b35537_z.jpg'},
+    {name: 'Angry', imgSrc: 'img/4154543904_6e2428c421_z.jpg'}
+  ]);
 
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() + 1);
