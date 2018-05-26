6395afdb129467494a4ce8aaf735d805b898d2a5
Elia Lima <eliaml777@gmail.com>
2018-05-26 20:03:13 -0300

    When you click the cat name it changes to the current Cat and changes all info on the page

--- js/app.js
+++ js/app.js
@@ -1,14 +1,31 @@
-var Cat = function(){
-  this.clickCount = ko.observable(0);
-  this.title = ko.observable('Tabby');
-  this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-  this.catArray = ko.observableArray([
-    {name: 'Tabby', imgSrc: 'img/434164568_fea0ad4013_z.jpg'},
-    {name: 'Mr. T', imgSrc: 'img/22252709_010df3379e_z.jpg'},
-    {name: 'T. Bone', imgSrc: 'img/1413379559_412a540d29_z.jpg'},
-    {name: 'Sleepyhead', imgSrc: 'img/9648464288_2516b35537_z.jpg'},
-    {name: 'Angry', imgSrc: 'img/4154543904_6e2428c421_z.jpg'}
-  ]);
+var initiaCats = [
+  {
+    clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/434164568_fea0ad4013_z.jpg'
+  },
+  {
+    clickCount: 0,
+    name: 'T-Bone',
+    imgSrc: 'img/22252709_010df3379e_z.jpg'
+  },
+  {
+    clickCount: 0,
+    name: 'Sleepyhead',
+    imgSrc: 'img/9648464288_2516b35537_z.jpg'
+  },
+  {
+    clickCount: 0,
+    name: 'Angry',
+    imgSrc: 'img/4154543904_6e2428c421_z.jpg'
+  }
+]
+
+var Cat = function(data){
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.catArray = ko.observableArray([data.catArray]);
 
   this.level = ko.computed(function() {
     if (this.clickCount() < 10) {
@@ -22,10 +39,21 @@ var Cat = function(){
 }
 
 var viewModel = function() {
-  this.currentCat = ko.observable(new Cat());
+  var self = this;
+  this.catList = ko.observableArray([]);
+
+  initiaCats.forEach(function(catItem){
+    self.catList.push(new Cat(catItem));
+  });
+
+  this.currentCat = ko.observable(this.catList()[0]);
 
   this.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1);
+    self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+  };
+
+  this.setCurrentCat = function(clickedCat) {
+    self.currentCat(clickedCat);
   };
 }
 
